b0aa22bc210dddcee8b0bc209d6793fa42caced9
aditya patel <aditya.patel@nonceblox.com>
2022-07-31 00:05:04 +0530

    update: header component

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -8,6 +8,7 @@ import Container from "@mui/material/Container";
 import Tooltip from "@mui/material/Tooltip";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import FolderIcon from "@mui/icons-material/Folder";
 
 const Header = () => {
   return (
@@ -70,6 +71,7 @@ const Header = () => {
                 sx={{
                   fontSize: "55px",
                   color: "#4481eb",
+                  mr: 3,
                   transitions: "all .2s ease-in",
                   "&:hover": {
                     color: "#04befe",
@@ -83,6 +85,30 @@ const Header = () => {
                 onClick={() =>
                   window.open(
                     `https://www.linkedin.com/in/adityap314/`,
+                    "_blank"
+                  )
+                }
+              />
+            </Tooltip>
+
+            <Tooltip title='Project Repo'>
+              <FolderIcon
+                sx={{
+                  fontSize: "55px",
+                  color: "#4481eb",
+                  transitions: "all .2s ease-in",
+                  "&:hover": {
+                    color: "#04befe",
+                    transform: "scale(1.02)",
+                  },
+                  "&:active": {
+                    transform: "scale(0.97)",
+                  },
+                  transform: "translateY(5px)",
+                }}
+                onClick={() =>
+                  window.open(
+                    `https://github.com/adiCacti/dsa-questions-tracker`,
                     "_blank"
                   )
                 }
