079f57e248101b58ed9443365993937b9ba0ec22
aditya patel <aditya.patel@nonceblox.com>
2022-07-30 19:49:06 +0530

    create & update: FileDropzone component

---
+++ src/components/common/FileDropzone.tsx
@@ -0,0 +1,86 @@
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import { useDropzone } from "react-dropzone";
+interface FileDropzoneProps {
+  setFileToImport: any;
+}
+
+const baseStyle = {
+  flex: 1,
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  padding: "20px",
+  borderWidth: 2,
+  borderRadius: 2,
+  borderColor: "#eeeeee",
+  borderStyle: "dashed",
+  backgroundColor: "#fafafa",
+  color: "#bdbdbd",
+  outline: "none",
+  transition: "border .24s ease-in-out",
+};
+
+const focusedStyle = {
+  borderColor: "#2196f3",
+};
+
+const acceptStyle = {
+  borderColor: "#00e676",
+};
+
+const rejectStyle = {
+  borderColor: "#ff1744",
+};
+
+function FileDropzone({ setFileToImport }: FileDropzoneProps) {
+  const [files, setFiles] = useState<any>();
+
+  const { getRootProps, getInputProps, isFocused, isDragAccept, isDragReject } =
+    useDropzone({
+      accept: { "application/json": [] },
+      maxFiles: 1,
+      onDrop: (acceptedFiles) => {
+        setFiles(
+          acceptedFiles.map((file) =>
+            Object.assign(file, {
+              preview: URL.createObjectURL(file),
+            })
+          )
+        );
+      },
+    });
+
+  const getJsonFile = useCallback(async () => {
+    if (!files) return;
+    let blob = await fetch(files[0]?.preview).then((r) => r.blob());
+
+    const data = JSON.parse(await blob.text());
+
+    setFileToImport(data);
+  }, [files, setFileToImport]);
+
+  useEffect(() => {
+    getJsonFile();
+  }, [getJsonFile]);
+
+  const style = useMemo(
+    () => ({
+      ...baseStyle,
+      ...(isFocused ? focusedStyle : {}),
+      ...(isDragAccept ? acceptStyle : {}),
+      ...(isDragReject ? rejectStyle : {}),
+    }),
+    [isFocused, isDragAccept, isDragReject]
+  );
+
+  return (
+    <div className='container'>
+      <div {...getRootProps({ style })}>
+        <input {...getInputProps()} />
+        <p>Drag n drop .json files here and then click on import</p>
+      </div>
+    </div>
+  );
+}
+
+export default FileDropzone;
